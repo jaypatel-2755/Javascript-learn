d8f8bb9e1daf733931c3aa83b66c674b37a9d322
Jay Patel <jayrpatel008@gmail.com>
2026-04-19 05:19:44 +0000

    DataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Jay")
+
+let name = "Jay"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Jay");
+console.log(typeof null); //Object
+console.log(typeof undefined); // undefined
+
